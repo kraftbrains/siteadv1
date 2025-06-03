32c63648148a176f9d5f2627ef2aaaf7772416d3
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-03 00:03:13 -0300

    Update configuration and structure for Next.js application with base path and asset prefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/siteadv1',
+  assetPrefix: '/siteadv1/',
   /* config options here */
 };
 
