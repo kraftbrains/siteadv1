23e4d52596a33d210877967251dda42de6df1800
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-03 11:23:23 -0300

    Update Next.js configuration; enable unoptimized images and set basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
+  images:{
+    unoptimized: true,
+  },
+  basePath: "/siteadv1"
 };
-
-export default nextConfig;
